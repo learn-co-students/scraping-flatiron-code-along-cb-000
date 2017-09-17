938a0844a6ae435ec6951927c8fb082bf6acf14b
Mohammad Khan <sparrowangels@gmail.com>
2017-09-17 20:24:29 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,20 @@
+# require 'nokogiri'
+# require 'open-uri'
 
+class Course
+  attr_accessor :title,:schedule,:description
+  @@all = []
 
+  def initialize()
+    @@all << self
+  end #initialize
+  # ***********************************************************************self
+  def self.all
+    # @@all << self
+    @@all
+  end #self.all
+
+  def self.reset_all
+    @@all.clear
+  end #reset
+end #Course
