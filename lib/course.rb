4ae3bcd924ad0a4573bcdace7132ed87545a0996
Jen Poole <contact.vandepool@gmail.com>
2018-02-02 22:54:50 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,14 @@
+class Course
+  # instance methods
+  # has a setter and a getter method for title
+  # has a setter and a getter method for a course's schedule
+  # has a setter and a getter method for a course's description
+  attr_accessor :title, :schedule, :description
 
 
+end
+
+
+# class methods
+  #.all
+    # returns and array of all the instances of the Course class
