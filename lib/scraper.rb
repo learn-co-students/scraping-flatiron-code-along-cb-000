78dfc1c135b6105252c715bbe399d32b5231abf4
Jen Poole <contact.vandepool@gmail.com>
2018-02-02 23:48:00 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,21 +5,13 @@ require 'pry'
 require_relative './course.rb'
 
 class Scraper
+  # For each iteration over the collection of Nokogiri XML elements returned to us by the doc.css(".post") line, we are making a new instance of the Course class and giving that instance the title, schedule and description extracted from the XML
 
   #get_page
   # use Nokogiri to get the HTML from a web page
-  # For each iteration over the collection of Nokogiri XML elements returned to us by the doc.css(".post") line, we are making a new instance of the Course class and giving that instance the title, schedule and description extracted from the XML
+  # This method should contain only the code for getting the HTML document
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-
-    # 1. grab an array-like collection of course elements from the page
-    doc.css("post").each do |post|
-      course = Course.new # 2. make a new Course instance for each course
-      course.title = post.css("h2").text # 3. assign scraped title to Course instance
-      course.schedule = post.css(".date").text # 4. assign scraped schedule to Course instance
-      course.description = post.css("p").text # 5. assign scraped description to Course instance
-    end
-    binding.pry
   end
 
   #print_courses
@@ -46,3 +38,14 @@ end
 
 #make_courses
 # iterates over the courses array returned by #get_courses and creates a new Course instance out of each array element
+
+
+=begin
+# 1. grab an array-like collection of course elements from the page
+doc.css("post").each do |post|
+  course = Course.new # 2. make a new Course instance for each course
+  course.title = post.css("h2").text # 3. assign scraped title to Course instance
+  course.schedule = post.css(".date").text # 4. assign scraped schedule to Course instance
+  course.description = post.css("p").text # 5. assign scraped description to Course instance
+end
+binding.pry
